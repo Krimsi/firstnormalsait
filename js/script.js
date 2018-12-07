fb869ef8137d51e5e7d5ed37472964b4049efd79
Krimsi <astt509@mail.ru>
2018-12-07 17:48:28 +0300

    sait

---
+++ js/script.js
@@ -0,0 +1,93 @@
+
+$('a[href^="#dima1"]').bind('click.smoothscroll',function (e) {
+e.preventDefault();
+
+var target = this.hash,
+$target = $(target);
+
+$('html, body').stop().animate({
+'scrollTop': $target.offset().top
+}, 1500, 'swing', function () {
+window.location.hash = target;
+});
+});
+
+
+
+$('a[href^="#dima2"]').bind('click.smoothscroll',function (e) {
+e.preventDefault();
+
+var target = this.hash,
+$target = $(target);
+
+$('html, body').stop().animate({
+'scrollTop': $target.offset().top
+}, 1500, 'swing', function () {
+window.location.hash = target;
+});
+});
+
+
+$('a[href^="#dima3"]').bind('click.smoothscroll',function (e) {
+e.preventDefault();
+
+var target = this.hash,
+$target = $(target);
+
+$('html, body').stop().animate({
+'scrollTop': $target.offset().top
+}, 1500, 'swing', function () {
+window.location.hash = target;
+});
+});
+
+
+
+$('a[href^="#dima4"]').bind('click.smoothscroll',function (e) {
+e.preventDefault();
+
+var target = this.hash,
+$target = $(target);
+
+$('html, body').stop().animate({
+'scrollTop': $target.offset().top
+}, 1500, 'swing', function () {
+window.location.hash = target;
+});
+});
+
+
+
+$('a[href^="#dima5"]').bind('click.smoothscroll',function (e) {
+e.preventDefault();
+
+var target = this.hash,
+$target = $(target);
+
+$('html, body').stop().animate({
+'scrollTop': $target.offset().top
+}, 1500, 'swing', function () {
+window.location.hash = target;
+});
+});
+
+
+$('a[href^="#vniz"]').bind('click.smoothscroll',function (e) {
+e.preventDefault();
+
+var target = this.hash,
+$target = $(target);
+
+$('html, body').stop().animate({
+'scrollTop': $target.offset().top
+}, 1500, 'swing', function () {
+window.location.hash = target;
+});
+});
+
+
+
+
+
+
+
